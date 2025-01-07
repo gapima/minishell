72509325e456587d107440102f0df441d1a4e527
gapima <gapima7@gmail.com>
2025-01-07 18:59:34 -0300

    norm

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: glima <gapima7@gmail.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 18:16:36 by glima             #+#    #+#             */
-/*   Updated: 2024/12/29 18:36:53 by glima            ###   ########.fr       */
+/*   Updated: 2025/01/07 18:58:56 by glima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-volatile int g_last_signal = 0;
+volatile int	g_last_signal = 0;
 
 void	shellzin_evaluate(char *line, t_shellzin *shell)
 {
@@ -21,16 +21,15 @@ void	shellzin_evaluate(char *line, t_shellzin *shell)
 	ast = parse(line, shell);
 	if (shell->parser.has_error)
 	{
-		ft_printf_fd(2, "%s near `%s`\n",
-						shell->parser.error_msg,
-						get_token_symbol(parser_peek_last(&shell->parser)));
+		ft_printf_fd(2, "%s near `%s`\n", shell->parser.error_msg, \
+		get_token_symbol(parser_peek_last(&shell->parser)));
 		if (shell->parser.error_from_lexer)
 			parser_free_token_list(&shell->parser);
 	}
 	else if (!parser_iseof(&shell->parser))
 	{
-		ft_printf_fd(2, "shellzin: syntax error: Unexpected token near `%s`\n",
-						get_token_symbol(parser_peek(&shell->parser)));
+		ft_printf_fd(2, "shellzin: syntax error: Unexpected token near `%s`\n", \
+		get_token_symbol(parser_peek(&shell->parser)));
 	}
 	else if (ast)
 	{
@@ -73,62 +72,6 @@ int	main(int ac, char *av[], char *envp[])
 	(void)ac;
 	(void)av;
 	shellzin_init(&shell, envp);
-	/*shellzin_evaluate("ls -al \"./libft\"", &shell);*/
-	/*shellzin_evaluate("touch \"a\" 'b' c", &shell);*/
-	/*shellzin_evaluate("\"ls\" \"-al\"", &shell);*/
-	/*shellzin_evaluate("rm -f a b c", &shell);*/
-	/*shellzin_evaluate("touch \"a\"\"b\"\"c\" ", &shell);*/
-	/*shellzin_evaluate("'ls'", &shell);*/
-	/*shellzin_evaluate("rm -f 'a'\"b\"\"c\" ", &shell);*/
-	/*shellzin_evaluate("'ls'", &shell);*/
-	/*shellzin_evaluate("touch ls \"libft\"\"a\"\"b\"\"", &shell);*/
-	/*shellzin_evaluate("echo \'\"a\"\'b\'\'c\'\"d\"\'", &shell);*/
-	/*shellzin_evaluate("echo \"$HOME $HOME $HOME '$HOME'\" '$HOME'", &shell);*/
-	/*shellzin_evaluate("\"\"", &shell);*/
-	/*shellzin_evaluate("''", &shell);*/
-	/*shellzin_evaluate("\"\"\"\"\"\"\"\"", &shell);*/
-	/*shellzin_evaluate("\"", &shell);*/
-	/*shellzin_evaluate("echo 'ok' 'a' 'b", &shell);*/
-	/*shellzin_evaluate("\"\"\"'$HOME '$HOME' $HOME $HOME'\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"", &shell);*/
-	/*shellzin_evaluate("\"\"\"\"\"\"\"\"\"\"\"\"\"'$HOME '$HOME' $HOME $HOME'\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"", &shell);*/
-	/*shellzin_evaluate("\"\"\"\"\"\"\"\"\"\"\"\"\"\"'$HOME '$HOME' $HOME $HOME'\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"", &shell);*/
-	/*shellzin_evaluate("echo a > b c d", &shell);*/
-	/*shellzin_evaluate("cat b", &shell);*/
-	/*shellzin_evaluate("rm -f ./b", &shell);*/
-	/*shellzin_evaluate("echo po>oi<oi>tst", &shell);*/
-	/*shellzin_evaluate("cat tst", &shell);*/
-	/*shellzin_evaluate("rm -f ./oi ./tst", &shell);*/
-	/*shellzin_evaluate("ls -al | grep sh", &shell);*/
-	/*shellzin_evaluate("ls -al > file", &shell);*/
-	/*shellzin_evaluate("cat file", &shell);*/
-	/*shellzin_evaluate("rm -f ./file", &shell);*/
-	/*shellzin_evaluate("ls >", &shell);*/
-	/*shellzin_evaluate("> file ls -al", &shell);*/
-	/*shellzin_evaluate("cat file", &shell);*/
-	/*shellzin_evaluate("rm -f file", &shell);*/
-	/*shellzin_evaluate("cd ./src/builtin", &shell);*/
-	/*shellzin_evaluate("ls", &shell);*/
-	/*shellzin_evaluate("cd", &shell);*/
-	/*shellzin_evaluate("ls", &shell);*/
-	/*shellzin_evaluate("cd $OLDPWD", &shell);*/
-	/*shellzin_evaluate("ls", &shell);*/
-	/*shellzin_evaluate("cd /home/pineappleforest/Public/gapima/minishell/../../../Public/gapima/minishell/", &shell);*/
-	/*shellzin_evaluate("cd ..", &shell);*/
-	/*shellzin_evaluate("cd libft", &shell);*/
-	/*shellzin_evaluate("pwd", &shell);*/
-	/*shellzin_evaluate("cat < Makefile", &shell);*/
-	/*shellzin_evaluate("export a\b=b", &shell);*/
-	/*shellzin_evaluate("export TEST1=A", &shell);*/
-	/*shellzin_evaluate("env | grep TEST", &shell);*/
-	/*shellzin_evaluate("unset TEST1", &shell);*/
-	/*shellzin_evaluate("env | grep TEST", &shell);*/
-	/*shellzin_evaluate("env", &shell);*/
-	/*shellzin_evaluate("echo '> >> < * ? [ ] | ; [ ] || && ( ) & # $  <<' | cat -e", &shell);*/
-	/*shellzin_evaluate("export =", &shell);*/
-	/*shellzin_evaluate("echo $?HELLO", &shell);*/
-	/*shellzin_evaluate("exit", &shell);*/
-	/*shellzin_evaluate("cat -e << EOF | grep line", &shell);*/
-	/*printf("%d\n", getpid());*/
 	shellzin_repl(&shell);
 	shellzin_deinit(&shell);
 }
