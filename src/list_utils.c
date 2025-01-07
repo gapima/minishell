f0faf5e8a210f708c74327c9e7341a37cef7a5d3
gapima <gapima7@gmail.com>
2025-01-07 18:56:28 -0300

    norm

--- src/list_utils.c
+++ src/list_utils.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glima <gapima7@gmail.com>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/07 18:54:38 by glima             #+#    #+#             */
+/*   Updated: 2025/01/07 18:55:48 by glima            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/minishell.h"
 
-char **join_string_list(t_list *list)
+char	**join_string_list(t_list *list)
 {
-	char **str_list;
-	size_t idx;
+	char	**str_list;
+	size_t	idx;
 
 	str_list = ft_calloc(ft_lstsize(list) + 1, sizeof(char *));
 	shellzin_assert(str_list != NULL, "(ft_realloc) could not allocate memory");
@@ -17,11 +29,11 @@ char **join_string_list(t_list *list)
 	return (str_list);
 }
 
-char **join_word_list(t_list *list)
+char	**join_word_list(t_list *list)
 {
-	char **str_list;
-	t_ast *node;
-	size_t idx;
+	char	**str_list;
+	t_ast	*node;
+	size_t	idx;
 
 	str_list = ft_calloc(ft_lstsize(list) + 1, sizeof(char *));
 	shellzin_assert(str_list != NULL, "(ft_realloc) could not allocate memory");
@@ -29,16 +41,16 @@ char **join_word_list(t_list *list)
 	while (list)
 	{
 		node = list->content;
-		str_list[idx++] = node->u_node.word_node.content; 
+		str_list[idx++] = node->u_node.word_node.content;
 		list = list->next;
 	}
 	str_list[idx] = NULL;
 	return (str_list);
 }
 
-void string_list_destroy(char **list)
+void	string_list_destroy(char **list)
 {
-	char **ptr;
+	char	**ptr;
 
 	ptr = list;
 	while (*ptr)
